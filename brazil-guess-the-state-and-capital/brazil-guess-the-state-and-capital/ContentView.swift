072b0452c99670435f2839668ddc998e1b0b8f7f
JeovaneSousa <jeovanesousa@icloud.com>
2023-02-28 15:33:42 -0300

    Project Setup

---
+++ brazil-guess-the-state-and-capital/brazil-guess-the-state-and-capital/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  brazil-guess-the-state-and-capital
+//
+//  Created by Jeovane Barbosa on 28/02/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ brazil-guess-the-state-and-capital/brazil-guess-the-state-and-capital/brazil_guess_the_state_and_capitalApp.swift
@@ -0,0 +1,17 @@
+//
+//  brazil_guess_the_state_and_capitalApp.swift
+//  brazil-guess-the-state-and-capital
+//
+//  Created by Jeovane Barbosa on 28/02/23.
+//
+
+import SwiftUI
+
+@main
+struct brazil_guess_the_state_and_capitalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
