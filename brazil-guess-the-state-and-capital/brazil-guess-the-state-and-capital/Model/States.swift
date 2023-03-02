906e902fca78de16f60bb5285e20e387f410677c
JeovaneSousa <jeovanesousa@icloud.com>
2023-03-02 13:08:05 -0300

    Finished project

---
+++ brazil-guess-the-state-and-capital/brazil-guess-the-state-and-capital/Model/States.swift
@@ -0,0 +1,68 @@
+//
+//  States.swift
+//  brazil-guess-the-state-and-capital
+//
+//  Created by Jeovane Barbosa on 01/03/23.
+//
+
+import Foundation
+
+class States {
+    
+    static let capitals = ["Acre": "Rio Branco",
+                           "Alagoas": "Maceio",
+                           "Amapa": "Macapa",
+                           "Amazonas": "Manaus",
+                           "Bahia": "Salvador",
+                           "Ceara": "Fortaleza",
+                           "Distrito Federal": "Brasilia",
+                           "Espirito Santo": "Vitoria",
+                           "Goias": "Goiania",
+                           "Maranhao": "Sao Luis",
+                           "Mato Grosso": "Cuiaba",
+                           "Mato Grosso do Sul": "Campo Grande",
+                           "Minas Gerais": "Belo Horizonte",
+                           "Para": "Belem",
+                           "Paraiba": "Joao Pessoa",
+                           "Parana": "Curitiba",
+                           "Pernambuco": "Recife",
+                           "Piaui": "Teresina",
+                           "Rio de Janeiro": "Rio de Janeiro",
+                           "Rio Grande do Norte": "Natal",
+                           "Rio Grande do Sul": "Porto Alegre",
+                           "Rondonia": "Porto Velho",
+                           "Roraima": "Boa Vista",
+                           "Santa Catarina": "Florianopolis",
+                           "Sao Paulo": "Sao Paulo",
+                           "Sergipe": "Aracaju",
+                           "Tocantins": "Palmas"]
+    
+    static let abbreviated = ["Acre": "AC",
+                         "Alagoas": "AL",
+                         "Amapa": "AP",
+                         "Amazonas": "AM",
+                         "Bahia": "BA",
+                         "Ceara": "CE",
+                         "Distrito Federal": "DF",
+                         "Espirito Santo": "ES",
+                         "Goias": "GO",
+                         "Maranhao": "MA",
+                         "Mato Grosso": "MT",
+                         "Mato Grosso do Sul": "MS",
+                         "Minas Gerais": "MG",
+                         "Para": "PA",
+                         "Paraiba": "PB",
+                         "Parana": "PR",
+                         "Pernambuco": "PE",
+                         "Piaui": "PI",
+                         "Rio de Janeiro": "RJ",
+                         "Rio Grande do Norte": "RN",
+                         "Rio Grande do Sul": "RS",
+                         "Rondonia": "RO",
+                         "Roraima": "RR",
+                         "Santa Catarina": "SC",
+                         "Sao Paulo": "SP",
+                         "Sergipe": "SE",
+                         "Tocantins": "TO"]
+    
+}
